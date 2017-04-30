681cba8961105ebae41016ad41f105fee8ebc892
cheli k <cheli k>
2017-04-30 19:39:49 +0300

    cover image size

--- plugins/bp-custom.php
+++ plugins/bp-custom.php
@@ -1,8 +1,9 @@
 <?php
-function your_theme_xprofile_cover_image( $settings = array() ) {
+function biskex_xprofile_cover_image( $settings = array() ) {
     $settings['width']  = 900;
     $settings['height'] = 380;
     return $settings;
 }
-add_filter( 'bp_before_xprofile_cover_image_settings_parse_args', 'your_theme_xprofile_cover_image', 10, 1 );
+add_filter( 'bp_before_xprofile_cover_image_settings_parse_args', 'biskex_xprofile_cover_image', 10, 1 );
+add_filter( 'bp_before_groups_cover_image_settings_parse_args', 'biskex_xprofile_cover_image', 10, 1 );
 ?>
